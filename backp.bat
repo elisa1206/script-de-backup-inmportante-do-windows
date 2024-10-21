773e1f623d08f202cce9cd765e5564e1629e879e
ELISANGELA PULIDO SANT ANNA <elisangela.psanna@senacsp.edu.br>
2024-10-21 16:29:56 -0300

    first commit

---
+++ backp.bat
@@ -0,0 +1,5 @@
+mkdir backup
+copy \windows\*.txt \Users\elisangela.psanna\script-backp\backup
+copy \windows\system32\*.log \users\elisangela,psanna\script-backp\backup
+copy \windows\system32\*.txt \Users\elisangela.psanna\script-backp\backup
+
